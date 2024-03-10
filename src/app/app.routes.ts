57f289fe1ee6c04e45dc3ffb011ae081764ae717
Thomas van de Meer <t.vandemeer@gmail.com>
2024-03-10 19:20:01 +0100

    Table met pagination. Combinatie met api nog niet gelukt.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { FrontendAccordionComponent } from './frontend-accordion/frontend-accordion.component';
 import { NavComponent } from './nav/nav.component';
 import { HexagonsComponent } from './hexagons/hexagons.component';
+import { PostsTableComponent } from './posts-table/posts-table.component';
 
 export const routes: Routes = [
     {path: '', component: NavComponent},
-    {path: 'hex', component: HexagonsComponent}
+    {path: 'hex', component: HexagonsComponent},
+    {path: 'posts', component: PostsTableComponent}
 ];
