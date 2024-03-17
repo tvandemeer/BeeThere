ce32ba3f71e24dbcbe074d110f2972eb96727531
Thomas van de Meer <t.vandemeer@gmail.com>
2024-03-17 11:09:49 +0100

    Info per module

--- src/app/module-detail/module-detail.component.ts
+++ src/app/module-detail/module-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MockbeeService } from '../services/mockbee.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-module-detail',
@@ -7,6 +10,29 @@ import { Component } from '@angular/core';
   templateUrl: './module-detail.component.html',
   styleUrl: './module-detail.component.css'
 })
-export class ModuleDetailComponent {
+export class ModuleDetailComponent implements OnInit {
+  
+  module: any = {};
+  //@Input() module: any;
+  
+  modDetail: any = {};
+
+  constructor(
+    private mockbeeService: MockbeeService,
+    private route: ActivatedRoute,
+    private location: Location) {}
+  
+  ngOnInit(): void {
+
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    console.log(id);
+
+    this.mockbeeService.GetModule(id)
+      .subscribe(res => this.module = res);
+  }
+
+  backClicked() {
+    this.location.back();
+  }
 
 }
